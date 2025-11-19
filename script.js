6bd5af98553331ca6684ce9c6f9282aba007ff6b
Manish kumar <169014705+manishkumar2543@users.noreply.github.com>
2025-11-19 20:44:21 +0530

    Day-4

--- script.js
+++ script.js
@@ -1,86 +1,79 @@
-// let h1=document.createElement("h1");
-// console.log(h1);
+// setTimeout - delay
+// setInterval - repeat
 
 
+// console.log("Start");
+// setTimeout(()=>{
+//     console.log("helo world")
+// },2000); // 2 seconds delay
 
-// createElement 💡
+// console.log("End");
+
 
 // let btn=document.querySelector("button");
-// btn.addEventListener("click",function(){
-//   let h1=document.createElement("h1");
-//   h1.innerText="Hello world";
-//     console.log(h1);
-  
-// })
-
-
-// appendChild 💡
-
-// let h1=document.createElement("h1");
-// h1.innerHTML="i love js";
-// let main=document.querySelector("main");
-// main.appendChild(h1);
-
-// let btn=document.createElement("button");
-// btn.innerText="New Button";
-// let main=document.querySelector("main");
-// main.appendChild(btn);
- 
-// let div=document.createElement("div");
-// div.style.height="100px";
-// div.style.width="100px";
-// div.style.backgroundColor="Yellow";
-// let main=document.querySelector("main");
-// main.appendChild(div);
-
-// let btn=document.querySelector("button");
-// let main=document.querySelector("main");
+// let h1=document.querySelector("h1");
 
 // btn.addEventListener("click",function(){
-//     let div=document.createElement("div");
-//     let x=Math.random()*100
-//     let y=Math.random()*100
-//     let c1=Math.floor(Math.random()*256);
-//     let c2=Math.floor(Math.random()*256);
-//     let c3=Math.floor(Math.random()*256);
-//     div.style.backgroundColor=`rgb(${c1},${c2},${c3})`;
-//     div.style.height="100px";
-//     div.style.width="100px";
-//     div.style.left= x+ '%';
-//     div.style.top= y+ '%';
-//     // div.style.backgroundColor="Yellow";
-//     div.style.position="absolute";
-//     main.appendChild(div);
+//     h1.innerHTML="Loading...";
+//     setTimeout(()=>{
+//         h1.innerHTML="Hello I am John";
+//     },2000)
 // })
 
 
 
+
+// let a=0;
+
+// setInterval(()=>{
+//     a++;
+//     console.log(a);
+// },2000)
+
+// let a=0;
+// setTimeout(function(){
+//     a++;
+//     console.log(a);
+// },500)
+
+
+// var a=0;
+
+// var int=setInterval(()=>{
+//     a++;
+//     console.log(a);
+// },50);
+
+// setTimeout(()=>{
+//     clearInterval(int);
+// },5000);
+
+
+let grow=0;
 let btn=document.querySelector("button");
-let main=document.querySelector("main");
+let h2=document.querySelector("h2");
+let inner=document.querySelector(".inner");
 
-let arr=['Hey! i am Manish Kumar','I love coding','Javascript is fun','DOM manipulation is easy','Lets create something new'];
+
 btn.addEventListener("click",function(){
-    let h1=document.createElement("h1");
-    let x=Math.random()*80
-    let y=Math.random()*80
-    let rot=Math.random()*360;
-    let scl=Math.random()*3;
-    h1.style.rotate=rot + 'deg';
-    h1.style.scale=scl;
-    h1.style.left= x+ '%';
-    h1.style.top= y+ '%';
-    let a= Math.floor(Math.random()*arr.length);
-    h1.innerHTML=arr[a];
-    h1.style.position="absolute";
 
-    main.appendChild(h1);
+    btn.style.pointerEvents='none';
+
+    let num= 50 + Math.floor(Math.random());
+    console.log(num)
+
+   let int = setInterval(() => {
+        grow++;
+        h2.innerHTML=grow+"%";
+        inner.style.width=grow+"%";
+    }, num);
+
+    setTimeout(()=>{
+        clearInterval(int);
+        btn.innerHTM="Downloaded";
+        
+         console.log('Downloaded in ',num/10,`Seconds`)
+    },num*100);
+
+   
 })
-
-
-
-
-
-// CreateElement is used to create new element
-// appendChild is used to append it any parent.
-
-
