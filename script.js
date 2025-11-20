21d4dafb6091fbce79203757c9c352c9f47ffe79
Manish kumar <169014705+manishkumar2543@users.noreply.github.com>
2025-11-20 17:09:48 +0530

    Day-5
    Instgram like effect on dblclick

--- script.js
+++ script.js
@@ -1,79 +1,20 @@
-// setTimeout - delay
-// setInterval - repeat
+let img =document.querySelector("img")
+let love=document.querySelector("#love")
 
+img.addEventListener("dblclick",function(){
+      love.style.opacity=1
+    love.style.transform=' translate(-50%,-50%) scale(1) rotate(0)'
+   setTimeout(() => {
+    
+    love.style.transform=' translate(-50%,-300%) scale(1) rotate(0)'
+   },800);
 
-// console.log("Start");
-// setTimeout(()=>{
-//     console.log("helo world")
-// },2000); // 2 seconds delay
+      setTimeout(() => {
+     love.style.opacity=0
+   },1000);
 
-// console.log("End");
-
-
-// let btn=document.querySelector("button");
-// let h1=document.querySelector("h1");
-
-// btn.addEventListener("click",function(){
-//     h1.innerHTML="Loading...";
-//     setTimeout(()=>{
-//         h1.innerHTML="Hello I am John";
-//     },2000)
-// })
-
-
-
-
-// let a=0;
-
-// setInterval(()=>{
-//     a++;
-//     console.log(a);
-// },2000)
-
-// let a=0;
-// setTimeout(function(){
-//     a++;
-//     console.log(a);
-// },500)
-
-
-// var a=0;
-
-// var int=setInterval(()=>{
-//     a++;
-//     console.log(a);
-// },50);
-
-// setTimeout(()=>{
-//     clearInterval(int);
-// },5000);
-
-
-let grow=0;
-let btn=document.querySelector("button");
-let h2=document.querySelector("h2");
-let inner=document.querySelector(".inner");
-
-
-btn.addEventListener("click",function(){
-
-    btn.style.pointerEvents='none';
-
-    let num= 50 + Math.floor(Math.random());
-    console.log(num)
-
-   let int = setInterval(() => {
-        grow++;
-        h2.innerHTML=grow+"%";
-        inner.style.width=grow+"%";
-    }, num);
-
-    setTimeout(()=>{
-        clearInterval(int);
-        btn.innerHTM="Downloaded";
-        
-         console.log('Downloaded in ',num/10,`Seconds`)
-    },num*100);
-
-   
+   setTimeout(()=>{
+     love.style.transform=' translate(-50%,-50%) scale(1) rotate(0)'
+   },1200)
+    
 })
