2c6421972cc332f4f65ebe1285ec8861d8c90c50
Manish kumar <169014705+manishkumar2543@users.noreply.github.com>
2025-11-26 21:23:22 +0530

    Day-6

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
-let img =document.querySelector("img")
-let love=document.querySelector("#love")
+// let box=document.querySelector("#box");
 
-img.addEventListener("dblclick",function(){
-      love.style.opacity=1
-    love.style.transform=' translate(-50%,-50%) scale(1) rotate(0)'
-   setTimeout(() => {
-    
-    love.style.transform=' translate(-50%,-300%) scale(1) rotate(0)'
-   },800);
+// box.addEventListener("mouseenter",function(){
+//     console.log("enter..")
+//     box.style.backgroundC
+// })
 
-      setTimeout(() => {
-     love.style.opacity=0
-   },1000);
+// box.addEventListener("mouseleave",function(){
+//     console.log("left ...")
+// })
 
-   setTimeout(()=>{
-     love.style.transform=' translate(-50%,-50%) scale(1) rotate(0)'
-   },1200)
-    
+// let chutki=document.querySelector('img');
+// let msg=document.querySelector('h2 span');
+// let body=document.body
+
+
+// chutki.addEventListener('mouseenter',function(){
+//     msg.innerHTML='chutki se door ho jaaa'
+//     body.style.backgroundColor='red'
+// })
+
+// chutki.addEventListener('mouseleave',function(){
+//    msg.innerHTML='Good! Ab door hi rahna'
+//    body.style.backgroundColor='black'
+// })
+
+
+// let main=document.querySelector('.main');
+// let cursor=document.querySelector('#cursor');
+
+// main.addEventListener('mousemove',function(dets){
+//     cursor.style.left=dets.x + 'px'
+//     cursor.style.top=dets.y + 'px'
+// })
+
+let aud=new Audio('D.mp3')
+
+let h1=document.querySelector("h1");
+
+window.addEventListener('keydown',function(dets){
+    // console.log(dets.key)
+     h1.innerHTML=dets.key
+
+     if(h1.innerHTML===" "){
+        h1.innerHTML='SPC'
+     }
+     if(h1.innerHTML==="d"){
+        aud.play()
+     }
+   
+        
 })
